8dc1737f2647c69eda55f856e92d1eba2a1d0d18
Constantin Orasan <dinel@dinel.org.uk>
2017-02-10 17:34:35 +0000

    Added \ to the characters to be ignored when a frequency list is produced

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -478,7 +478,7 @@ class DefaultController extends Controller
             $a_text = $tokenizer->tokenize($feedback->getText());
             foreach ($a_text as $word) {
                 if(array_key_exists(strtolower($word), $stopwords) ||
-                   stristr(",.?!'’:“”-_–…()/", $word)) {
+                   stristr(",.?!'’:“”-_–…()/\\", $word)) {
                     continue;
                 }
                 
